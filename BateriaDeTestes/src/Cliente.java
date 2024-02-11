3f5485bc211913c481335e7728b64d37b93d0627
Juliana Cavalaro de Oliveira <juliana.cavalaro@hotmail.com>
2024-02-11 12:32:07 -0300

    Refatoração do Bad Smell

--- BateriaDeTestes/src/Cliente.java
+++ BateriaDeTestes/src/Cliente.java
@@ -1,33 +1,29 @@
 
 public class Cliente {
 
-		private boolean age;
-		private boolean sub;
-		private boolean notFTime;
+		private boolean _isSenior;
+		private boolean _isAlreadySubsidized;
+		private boolean _isPartTime;
 
 		public Cliente(boolean age, boolean sub, boolean notFTime) {
-			this.age = age;
-			this.sub = sub;
-			this.notFTime = notFTime;
+			this._isSenior = age;
+			this._isAlreadySubsidized = sub;
+			this._isPartTime = notFTime;
 		}
 		
 		public int calculateDiscount() {
 			int discount = 0;
-			if(!age ) {
-				if(!sub) {
-					if(!notFTime) {
-						discount = 500;
-					}else {
-						discount = 250;
-					}
-				}else {
-					discount = 250;
-				}
+			if(_isSenior ) {	discount = 0;
 			}else {
-				discount = 0;
+				if(_isAlreadySubsidized) {	discount = 250;
+				}else {
+					if(_isPartTime) { discount = 250;
+					}else {
+						discount = 500;
+					}
+				}
 			}
-			return discount;
-			
-		}
-
+				return discount;
+			}
 }
+		
